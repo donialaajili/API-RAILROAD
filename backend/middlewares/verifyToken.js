5e4ab00c33c2025a47ae766a0ec661dcd5b0bb89
Donna100 <donia.laajili@supinfo.com>
2023-12-13 02:58:17 +0100

    update verifytoken

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import TrainStation from "../models/TrainStation"; // Update with the correct path and model name
+import TrainStation from "../models/TrainStation"; 
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
