1451edb3318dec4d4b4bcacf9355e0dbdc5b43ed
Donna100 <donia.laajili@supinfo.com>
2023-12-13 18:53:44 +0100

    update ticket and joi

--- backend/routes/ticket.js
+++ backend/routes/ticket.js
@@ -1,27 +1,45 @@
 import Ticket from './models/Ticket';
+import express from 'express';
+import Joi from 'joi';
 
-const bookTicket = async (ticketData, user) => {
-  try {
-   
-     const ticket = new Ticket({
-      startStation: ticketData.startStation,
-      endStation: ticketData.endStation,
-      date: ticketData.date,
-     
-      user: user._id, 
-    });
 
-    const savedTicket = await ticket.save();
-
+// Define the Joi schema for the ticket data
+const Ticket = Joi.object({
+    startStation: Joi.string().required(),
+    endStation: Joi.string().required(),
+    date: Joi.date().iso().required(),
+    // Add other relevant fields to validate
+  });
   
-    return { message: 'Ticket booked successfully', ticket: savedTicket };
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error booking ticket');
-  }
-};
 
-export default {
-    bookTicket,
-    
-};    
+router.post('/create-ticket', async (req, res) => {
+    try {
+      // Validate the request body against the schema
+      const { error, value } = Ticket.validate(req.body);
+  
+      if (error) {
+        // If validation fails, respond with a 400 Bad Request and error details
+        return res.status(400).json({ error: error.details[0].message });
+      }
+  
+      // If validation passes, create a new ticket using Mongoose
+      const ticket = new Ticket({
+        startStation: value.startStation,
+        endStation: value.endStation,
+        date: value.date,
+        // Add other relevant fields to create
+      });
+  
+      // Save the ticket to the database
+      const savedTicket = await ticket.save();
+  
+      // Respond with a success message and the created ticket
+      res.json({ message: 'Ticket created successfully', ticket: savedTicket });
+    } catch (error) {
+      // Handle any internal server errors
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  export default router;
